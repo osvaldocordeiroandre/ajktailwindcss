585e98e8181d586401ce0330b8d11526881c4559
Osvaldo Cordeiro André <94922391+osvaldocordeiroandre@users.noreply.github.com>
2024-04-24 03:22:37 -0300

    testando mais da responsividade de tailwind css

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { GiHamburgerMenu } from "react-icons/gi";
-import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowLeft, FaArrowRight  } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import { TbWorldShare } from "react-icons/tb";
+import { IoMdClose } from "react-icons/io";
+
+import ImageSkelet from '../headerLeft/ImageSkelet'
 
 export default function Content() {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [profileOpen, setProfileOpen] = useState(false)
+  
   return (
     <div className=' w-full pl-6 pt-10 flex justify-center overflow-y-scroll scrollbar-thin scrollbar-thumb-orange-400 scrollbar-track-zinc-800'>
       <div>
-        <div className='flex justify-between mx-4'>
+        <div className='hidden justify-between mx-4 sm:flex'>
           <div>
-            <GiHamburgerMenu/>
+            {profileOpen ? (
+              <IoMdClose onClick={() => setProfileOpen(false)} />
+            ) : <GiHamburgerMenu onClick={() => setProfileOpen(true)} />}
+          </div>
+          <div>
+            {menuOpen ? <FaArrowRight onClick={() => setMenuOpen(false)}/> : <FaArrowLeft onClick={() => setMenuOpen(true)} />}
           </div>
         </div>
+
+        {profileOpen && (
+                  <header className=' h-screen w-full hidden flex-col items-center border-l-8 border-orange-600 sm:flex fixed bg-zinc-800 animate-slide-Y'>
+                  <div className='pt-10 overflow-y-scroll'>
+                      <ImageSkelet hash={'LqONB[%Mof%M~qIUofRjof-;t7IU'} src={'https://i.ibb.co/ZgRp7Vy/4ae07d6ffec77bcfba4badb12d3ee5da.jpg'} />
+                      <div className='flex justify-center pt-4'>
+                          <span className='text-xl animate-slide-Y '>AJK</span>
+                      </div>
+      
+                      <nav className='pt-10 flex flex-col justify-between p-4'>
+                          <ul className='flex flex-col gap-4 text-2xl animate-slide-X text-center'>
+                              <li className='cursor-pointer'>sobre</li>
+                              <li className='cursor-pointer'>projetos</li>
+                              <li className='cursor-pointer'>site</li>
+                          </ul>
+                      </nav>
+                  </div>
+              </header>
+        )}
+
         <h1 className='text-4xl text-center animate-slide-Y'>ajk portfolio</h1>
 
         <div className='max-w-[700px] pt-20 w-full p-5'>
@@ -29,6 +63,25 @@ export default function Content() {
           <p className='animate-slide-Y'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aliquam quaerat dicta veniam ipsam repudiandae deserunt accusantium quibusdam, sunt repellendus quasi vero molestiae, enim eum amet blanditiis quia eaque eveniet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptatibus commodi aperiam amet nostrum voluptatum optio natus sunt rem nisi ab perferendis alias illo quos, laborum harum corrupti consequatur aspernatur?</p>
         </div>
       </div>
+      
+      {menuOpen && (
+          <aside className='bg-orange-600 h-screen w-[60px] text-white flex justify-center items-center overflow-hidden animate-slide-X transition-all z-40'>
+            <ul className='flex flex-col justify-center items-center gap-4 font-sans text-xl w-full'>
+              <a href="https://github.com/osvaldocordeiroandre" target='_blank'>
+                <li className='hover:bg-orange-400 w-full text-center cursor-pointer p-4 '><FaGithub size={30} /></li>
+              </a>
+              <a href="https://www.linkedin.com/in/osvaldo-cordeiro-andr%C3%A9-009760231/" target='_blank'>
+                <li className='hover:bg-orange-400 w-full text-center cursor-pointer p-4'><FaLinkedin size={30} /></li>
+              </a>
+              <a href="mailto:ajknew2022@gmail.com">
+                <li className='hover:bg-orange-400 w-full text-center cursor-pointer p-4'><MdOutlineEmail size={30} /></li>
+              </a>
+              <a href="https://itadoriamvs.vercel.app/" target='_blank'>
+                <li className='hover:bg-orange-400 w-full text-center cursor-pointer p-4'><TbWorldShare size={30} /></li>
+              </a>
+            </ul>
+          </aside>
+        )}
     </div>
   )
 }
